9fa606be3a11e46b41e2f3b36ad33f5e0181c963
vinayachandra <vinaychandra166@gmail.com>
2024-10-28 19:59:38 +0530

    added neets for personalised audios

---
+++ apis/tts.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
+
+
+def getAudioFromNeets(text : str, filename : str):
+    response = requests.request(
+        method="POST",
+        url="https://api.neets.ai/v1/tts",
+        headers={
+            "Content-Type": "application/json",
+            "X-API-Key": os.getenv("NEETS_API_KEY")
+        },
+        json={
+            "text": text,
+            "voice_id": "andrew-tate",
+            "params": {
+                "model": "ar-diff-50k"
+            }
+        }
+    )
+
+    with open(filename, "wb") as f:
+        f.write(response.content)
+
+    return filename
+
+
