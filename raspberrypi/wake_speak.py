ea8532c52b338db0e83a7d69080b70487659cc0c
vinayachandra <vinaychandra166@gmail.com>
2025-02-08 12:41:16 +0530

    changed to alexa

--- raspberrypi/wake_speak.py
+++ raspberrypi/wake_speak.py
@@ -33,7 +33,7 @@ def main():
     porcupine = pvporcupine.create(
         #access_key='pFaTLsJqVDE6/uSXFftK07tDKzwK6JaLnVAGH5nwvAl3W+oF6sO58Q==',
         access_key="lH2qVWwBT37IcqqB3rJEhEinpqAyx6UA/lmD40s6CJ71uUby4jcaXA==",
-        keywords=['snowboy','jarvis', 'computer']
+        keywords=['alexa','jarvis', 'computer']
     )
     
 
